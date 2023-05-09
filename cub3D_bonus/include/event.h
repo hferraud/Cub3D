440737036b6d8448c9a27fc300e59a37f4fafab0
edelage <ethandelage04@gmail.com>
2023-05-10 01:15:26 +0200

    Add add_damage_event

--- cub3D_bonus/include/event.h
+++ cub3D_bonus/include/event.h
@@ -14,6 +14,7 @@
 
 # include "vector.h"
 # include "libft.h"
+# include "weapons_data.h"
 
 typedef struct s_event			t_event;
 typedef enum e_event_id			t_event_id;
@@ -22,20 +23,24 @@ enum	e_event_id
 {
 	EVENT_OPEN_DOOR,
 	EVENT_CLOSE_DOOR,
-	EVENT_DEATH,
 	EVENT_TAKE_COLLECTIBLE,
+	EVENT_DAMAGE,
+	EVENT_DEATH,
 };
 
 struct s_event
 {
 	t_event_id	id;
 	t_vector	position;
+	int			enemy_id;
+	t_damage	damage;
 };
 
 typedef struct s_cub	t_cub;
 
 t_list	*new_event(t_event event_data);
 void	add_collectible_event(t_cub *cub);
+void	add_damage_event(t_cub *cub, int enemy_id, t_damage damage);
 void	add_door_event(t_cub *cub, t_vector map_pos, char door_state);
 
 #endif
