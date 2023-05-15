cd99ab9984f4d3e0f499f4602d94fc4e2b777328
= <ethandelage04@gmail.com>
2023-05-15 03:21:05 +0200

    Norm player_movement

--- cub3D_bonus/src/client/player/player_movement.c
+++ cub3D_bonus/src/client/player/player_movement.c
@@ -17,7 +17,10 @@ void				collectible_set_dist(t_cub *cub, t_player player);
 void				collectible_sort(t_cub *cub);
 
 static void			player_rotation_update(t_cub *cub);
+static void			mouse_rotation(t_cub *cub);
 static void			player_position_update(t_cub *cub);
+static void			update_position_on_collision(t_cub *cub, char **map,
+						t_player *player, t_fvector new_pos);
 
 /**
  * @brief Update the position and rotation of the player
@@ -49,22 +52,40 @@ void	player_update(t_cub *cub)
 
 static void	player_rotation_update(t_cub *cub)
 {
-	static size_t	count = 0;
-	int 			x;
-	int 			y;
-
-	mlx_mouse_get_pos(cub->mlx_data->mlx_ptr, cub->mlx_data->win_ptr, &x, &y);
-	if ((!cub->mlx_data->mouse_disable && x != WIN_WIDTH / 2)
-		|| is_key_pressed(KEY_LEFT, cub)
+	if (is_key_pressed(KEY_LEFT, cub)
 		|| is_key_pressed(KEY_RIGHT, cub))
 	{
-		if (!cub->mlx_data->mouse_disable && x != WIN_WIDTH / 2)
-			cub->player_data.player.rotation = fvector_rotate(cub->player_data.player.rotation, PLAYER_ROTATION / 100.0f * (x - (WIN_WIDTH / 2)));
 		if (is_key_pressed(KEY_LEFT, cub))
-			cub->player_data.player.rotation = fvector_rotate(cub->player_data.player.rotation, -PLAYER_ROTATION);
+			cub->player_data.player.rotation
+				= fvector_rotate(cub->player_data.player.rotation,
+					-PLAYER_ROTATION);
 		if (is_key_pressed(KEY_RIGHT, cub))
-			cub->player_data.player.rotation = fvector_rotate(cub->player_data.player.rotation, PLAYER_ROTATION);
-		cub->player_data.camera = fvector_rotate(cub->player_data.player.rotation, M_PI_2);
+			cub->player_data.player.rotation
+				= fvector_rotate(cub->player_data.player.rotation,
+					PLAYER_ROTATION);
+		cub->player_data.camera
+			= fvector_rotate(cub->player_data.player.rotation, M_PI_2);
+	}
+	mouse_rotation(cub);
+}
+
+static void	mouse_rotation(t_cub *cub)
+{
+	static size_t	count = 0;
+	int				x;
+	int				y;
+
+	mlx_mouse_get_pos(cub->mlx_data->mlx_ptr, cub->mlx_data->win_ptr, &x, &y);
+	if (!cub->mlx_data->mouse_disable && x != WIN_WIDTH / 2)
+	{
+		if (!cub->mlx_data->mouse_disable && x != WIN_WIDTH / 2)
+		{
+			cub->player_data.player.rotation
+				= fvector_rotate(cub->player_data.player.rotation,
+					PLAYER_ROTATION / 100.0f * (x - (WIN_WIDTH / 2)));
+			cub->player_data.camera
+				= fvector_rotate(cub->player_data.player.rotation, M_PI_2);
+		}
 	}
 	if (!cub->mlx_data->mouse_disable && count % 2 == 0)
 		mlx_mouse_move(cub->mlx_data->mlx_ptr, cub->mlx_data->win_ptr,
@@ -87,30 +108,35 @@ static void	player_position_update(t_cub *cub)
 	if (is_valid_position(cub, new_pos.x, new_pos.y))
 		player->pos = new_pos;
 	else
+		update_position_on_collision(cub, map, player, new_pos);
+}
+
+static void	update_position_on_collision(t_cub *cub, char **map,
+				t_player *player, t_fvector new_pos)
+{
+	if (is_floor(map[(int)(new_pos.y - PLAYER_OFFSET)][(int) new_pos.x])
+		&& is_floor(map[(int)(new_pos.y + PLAYER_OFFSET)][(int) new_pos.x])
+		&& is_valid_position(cub, player->pos.x, new_pos.y))
 	{
-		if (is_floor(map[(int)(new_pos.y - PLAYER_OFFSET)][(int) new_pos.x])
-			&& is_floor(map[(int)(new_pos.y + PLAYER_OFFSET)][(int) new_pos.x])
-			&& is_valid_position(cub, player->pos.x, new_pos.y))
-		{
-			player->pos.y = new_pos.y;
-			if (new_pos.x > player->pos.x)
-				player->pos.x
-					= 1.f - UNCERTAINTY - PLAYER_OFFSET + ((int) player->pos.x);
-			else
-				player->pos.x
-					= PLAYER_OFFSET + UNCERTAINTY + ((int) player->pos.x);
-		}
-		else if (is_floor(map[(int) new_pos.y][(int)(new_pos.x + PLAYER_OFFSET)])
+		player->pos.y = new_pos.y;
+		if (new_pos.x > player->pos.x)
+			player->pos.x
+				= 1.f - UNCERTAINTY - PLAYER_OFFSET + ((int) player->pos.x);
+		else
+			player->pos.x
+				= PLAYER_OFFSET + UNCERTAINTY + ((int) player->pos.x);
+	}
+	else if (
+		is_floor(map[(int) new_pos.y][(int)(new_pos.x + PLAYER_OFFSET)])
 			&& is_floor(map[(int)new_pos.y][(int)(new_pos.x - PLAYER_OFFSET)])
 			&& is_valid_position(cub, new_pos.x, player->pos.y))
-		{
-			player->pos.x = new_pos.x;
-			if (new_pos.y > player->pos.y)
-				player->pos.y
-					= 1.f - UNCERTAINTY - PLAYER_OFFSET + ((int) player->pos.y);
-			else
-				player->pos.y
-					= PLAYER_OFFSET + UNCERTAINTY + ((int) player->pos.y);
-		}
+	{
+		player->pos.x = new_pos.x;
+		if (new_pos.y > player->pos.y)
+			player->pos.y
+				= 1.f - UNCERTAINTY - PLAYER_OFFSET + ((int) player->pos.y);
+		else
+			player->pos.y
+				= PLAYER_OFFSET + UNCERTAINTY + ((int) player->pos.y);
 	}
 }
