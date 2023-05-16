fc5de3102dbcbe577eb1ce3450a4311c0807ab02
Mururoahh <ferraudhugo@gmail.com>
2023-05-16 07:16:49 +0200

    norm mandatory part

--- cub3D/src/display/draw_shape/draw_background.c
+++ cub3D/src/display/draw_shape/draw_background.c
@@ -13,15 +13,21 @@
 
 void	draw_background(t_cub *cub)
 {
-    int floor_color;
-    int ceiling_color;
-    t_rectangle floor;
-    t_rectangle ceiling;
+	int			floor_color;
+	int			ceiling_color;
+	t_rectangle	floor;
+	t_rectangle	ceiling;
 
 	floor_color = cub->map->floor_color;
 	ceiling_color = cub->map->ceiling_color;
-	floor = set_rectangle(set_point(0, WIN_HEIGHT / 2 + 1), set_point(WIN_WIDTH - 1, WIN_HEIGHT - 1), floor_color);
-	ceiling = set_rectangle(set_point(0, 0), set_point(WIN_WIDTH - 1, WIN_HEIGHT / 2), ceiling_color);
+	floor = set_rectangle(
+			set_point(0, WIN_HEIGHT / 2 + 1),
+			set_point(WIN_WIDTH - 1, WIN_HEIGHT - 1),
+			floor_color);
+	ceiling = set_rectangle(
+			set_point(0, 0),
+			set_point(WIN_WIDTH - 1, WIN_HEIGHT / 2),
+			ceiling_color);
 	draw_rectangle(&cub->mlx_data->img_data, floor);
 	draw_rectangle(&cub->mlx_data->img_data, ceiling);
 }
